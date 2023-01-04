cb42d60452e5965455ad0cfe13a35b39e694a268
Vladimir Grichina <vgrichina@gmail.com>
2023-01-04 01:30:32 -0800

    Move dump-car to scripts/

--- scripts/dump-car.js
+++ scripts/dump-car.js
@@ -1,4 +1,4 @@
-const { readCAR, readBlock, cidToString, validateBlock } = require('./');
+const { readCAR, readBlock, cidToString, validateBlock } = require('../');
 
 // Print usage info if not enough args and exit
 if (process.argv.length < 3) {
